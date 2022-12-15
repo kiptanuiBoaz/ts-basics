b3e6162b1f7551ca737d78897b9e0e4713518112
kiptanui boaz <boaserem022@gmail.com>
2022-12-15 12:15:26 +0300

    added tuple

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,12 @@
 "use strict";
-let age = 20;
-if (age > 50) {
-    age + 10;
+let sales = 123456789;
+let course = "Typescript";
+let is_published = true;
+let noarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+function render(document) {
 }
+let user = [3, "bob",];
+;
+const mySize = 0 /* Size.Small */;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ let is_published: boolean = true;
 let noarr:number[] = [1,2,3,4,5,6,7,8,9,10,11,12]
 
 function render(document:any){
-    console.log(document)
 }
  let user:[number, string] = [3,"bob", ]
+
+ const enum Size {Small, Medium, Large};
+
+ const mySize : Size = Size.Small;
+ console.log(mySize);
