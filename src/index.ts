cb1b54bb7b29fe397f58a796e9beaf70b8da8dcb
kiptanui boaz <boaserem022@gmail.com>
2022-12-15 11:55:19 +0300

    added tuple

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 let sales: number = 123_456_789;
 let course: string = "Typescript";
 let is_published: boolean = true;
+let noarr:number[] = [1,2,3,4,5,6,7,8,9,10,11,12]
 
-function render(document){
-    console.log(document);
+function render(document:any){
+    console.log(document)
+}
+ let user:[number, string] = [3,"bob", ]
